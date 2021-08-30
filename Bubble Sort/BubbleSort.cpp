c4ba8b6d384601f9376eae9d6760c5d38046ac8b
Additya Akash Mishra <yadashesh@gmail.com>
2021-08-30 17:00:13 +0530

    Done

--- Bubble Sort/BubbleSort.cpp
+++ Bubble Sort/BubbleSort.cpp
@@ -34,6 +34,7 @@ int main()
 { 
     int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
     int n = sizeof(arr)/sizeof(arr[0]); 
+    printArray(arr, n); 
     bubbleSort(arr, n); 
     cout<<"Sorted array: \n"; 
     printArray(arr, n); 
